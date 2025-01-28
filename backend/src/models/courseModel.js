47513926a7b0d4e514d306336f9c8402b12c1131
gqbo <gabriel.gonzaf@gmail.com>
2025-01-28 14:42:41 -0600

    Delete the use of an UUID when inserting a course.

--- backend/src/models/courseModel.js
+++ backend/src/models/courseModel.js
@@ -8,16 +8,19 @@ export class CourseModel {
         await this.db.query('START TRANSACTION')
   
         await this.db.query(
-          `INSERT INTO user_courses (id, user_id, course_name, day, start_time, end_time, course_code, classroom, building) 
-          VALUES (UUID(), ?, ?, ?, ?, ?, ?, ?, ?)`,
+          `INSERT INTO user_courses (user_id, course_name, day, start_time, end_time, course_code, classroom, building) 
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
           [user_id, course_name, day, start_time, end_time, course_code, classroom, building]
         )
   
         await this.db.query('COMMIT')
   
-        return { message: 'Course added successfully', course: { user_id, course_name, day, start_time, end_time, course_code, classroom, building } }
+        return { message: 'Course added successfully', course: { course_name, day, start_time, end_time } }
       } catch (error) {
         await this.db.query('ROLLBACK')
+        if (error.code === 'ER_DUP_ENTRY') {
+          return { error: `You already have "${course_name}" already scheduled on ${day} at ${start_time}.` }
+        }
         throw error
       }
     }
