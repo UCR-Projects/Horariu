8a2fc97c4c3d6e48c1a41b1982e95d7235f071b7
gqbo <gabriel.gonzaf@gmail.com>
2025-01-28 00:30:07 -0600

    Delete query to insert a course and check if username exists.

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -25,11 +25,6 @@ export class UserModel {
         [email]
       )
 
-      await this.db.query(
-        'INSERT INTO courses (id, user_id) VALUES (UUID(), ?)',
-        [newUser[0].id]
-      )
-
       await this.db.query('COMMIT')
 
       return { id: newUser[0].id, email }
@@ -40,14 +35,6 @@ export class UserModel {
     }
   }
 
-  async usernameExists({ username }) {
-    const [userExists] = await this.db.query(
-      'SELECT COUNT(*) as count FROM users WHERE username = ?',
-      [username]
-    )
-    return userExists[0].count > 0
-  }
-
   async emailExists({ email }) {
     const [emailExists] = await this.db.query(
       'SELECT COUNT(*) as count FROM users WHERE email = ?',
@@ -56,10 +43,10 @@ export class UserModel {
     return emailExists[0].count > 0
   }
 
-  async verifyCredentials({ credential, password }) {
+  async verifyCredentials({ email, password }) {
     const [user] = await this.db.query(
       'SELECT id, email, password FROM users WHERE email = ?',
-      [credential]
+      [email]
     )
 
     if (!user.length) return null
@@ -67,7 +54,7 @@ export class UserModel {
     if (await bcrypt.compare(password, user[0].password)) {
       return { id: user[0].id, email: user[0].email }
     }
-    
+
     return null
   }
 }
