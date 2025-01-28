39ff0cd98dad0fc5de221c476d3d3093202c3028
gqbo <gabriel.gonzaf@gmail.com>
2025-01-28 02:28:54 -0600

    Add the courses router to the api router.

--- backend/src/routes/apiRoutes.js
+++ backend/src/routes/apiRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { createUsersRouter } from './usersRoutes.js'
+import { createCoursesRouter } from './coursesRoutes.js'
 
-export const createApiRouter = ({ userModel }) => {
+export const createApiRouter = ({ userModel, courseModel }) => {
     const apiRouter = Router()
     apiRouter.use('/users', createUsersRouter({ userModel }))
+    apiRouter.use('/courses', createCoursesRouter({ courseModel }))
     return apiRouter
 }
