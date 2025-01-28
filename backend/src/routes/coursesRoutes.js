fbb3d524d1f3486cab35a47c0b0693505c38fb93
gqbo <gabriel.gonzaf@gmail.com>
2025-01-28 14:41:06 -0600

    Add middleware to authenticate a user when a adding a course.

--- backend/src/routes/coursesRoutes.js
+++ backend/src/routes/coursesRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { CourseController } from '../controllers/courseController.js'
+import { authenticateUser } from '../middlewares/auth.js'
 
 export const createCoursesRouter = ({ courseModel }) => {
 
     const coursesRouter = Router()
     const courseController = new CourseController({ courseModel })
 
-    coursesRouter.post('/', courseController.registerCourse)
+    coursesRouter.post('/', authenticateUser, courseController.registerCourse)
 
     return coursesRouter
 }
