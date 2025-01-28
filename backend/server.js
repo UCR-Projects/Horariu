c226ff80788c59c2b8426e3c32529ba41cc03229
gqbo <gabriel.gonzaf@gmail.com>
2025-01-28 02:30:55 -0600

    Add course model integration to the application.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import { createApp, startApp } from './src/app.js'
 import { pool } from './src/config/database.js'
 import { UserModel } from './src/models/userModel.js'
+import { CourseModel } from './src/models/courseModel.js'
 
 const userModel = new UserModel(pool)
+const courseModel = new CourseModel(pool)
 
-const app = createApp({ userModel })
+const app = createApp({ userModel, courseModel })
 
 try {
   await startApp(app)
