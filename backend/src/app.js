de7843a1749164d854074ff7f45096350ebbd0a7
gqbo <gabriel.gonzaf@gmail.com>
2025-01-27 23:19:32 -0600

    Add usersRoutes to '/users' and start app async to make sure the DB is loaded.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,29 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import { createUsersRouter } from './routes/usersRoutes.js'
 
-export const createApp = () => {
+export const createApp = ({userModel}) => {
+    dotenv.config()
     const app = express()
 
     app.use(cors())
     app.use(express.json())
+
+    app.use('/users', createUsersRouter({ userModel }))
 
     app.get("/", (req, res) => {
         res.send("Welcome to the Horarius API!")
     })
 
     return app
+}
+
+export const startApp = async (app, port = process.env.PORT ?? 3000) => {
+    return new Promise((resolve) => {
+        const server = app.listen(port, () => {
+        console.log(`Server running on port http://localhost:${port}`)
+        resolve(server)
+        })
+    })
 }
