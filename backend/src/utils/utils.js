6e3941fed41dd4bb04738f52b814d6f02e2755f8
gqbo <gabriel.gonzaf@gmail.com>
2025-01-29 21:16:32 -0600

    Migrate utils from JS to TS.

--- backend/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export const signToken = (user) => {
-  return jwt.sign(
-    { user_id: user.id, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: '1h' }
-  )
-}
---
+++ backend/src/utils/utils.ts
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+interface UserPayload {
+  id: string
+  email: string
+}
+
+export const signToken = (user: UserPayload): string => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables")
+  }
+
+  return jwt.sign(
+    { user_id: user.id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  )
+}
