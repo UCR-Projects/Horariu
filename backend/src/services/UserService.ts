821c7467aff36757e303de7087c8798db9988e1b
gqbo <gabriel.gonzalezflores@ucr.ac.cr>
2025-03-06 12:49:13 -0600

    feat: refactor UserService to use UserCredentials for registration and login and remove validation

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,43 +1,28 @@
-import { validateUser, validateLogin } from '../schemas/user.schema'
+import { UserCredentials } from '../schemas/user.schema'
 import { signToken } from '../utils/utils'
 import { UserRepository } from '../repositories/userRepository'
 
-interface IUserService {
-  register(data: unknown): Promise<{ token: string }>;
-  login(data: unknown): Promise<{ token: string }>;
-}
-
-export const UserService: IUserService = {
-  async register (data: unknown): Promise<{ token: string }> {
-    const result = await validateUser(data)
-    if (!result.success) {
-      throw new Error(JSON.stringify(result.error.format()))
-    }
-
-    const emailExists = await UserRepository.emailExists(result.data.email)
+export const UserService = {
+  async register (user: UserCredentials): Promise<{ token: string }> {
+    const emailExists = await UserRepository.emailExists(user.email)
     if (emailExists) {
       throw new Error('Email already exists')
     }
 
-    const newUser = await UserRepository.createUser(result.data)
+    const newUser = await UserRepository.createUser(user)
 
     const token = signToken(newUser)
 
     return { token }
   },
 
-  async login (data: unknown): Promise<{ token: string }> {
-    const result = await validateLogin(data)
-    if (!result.success) {
-      throw new Error(JSON.stringify(result.error.format()))
-    }
-
-    const user = await UserRepository.verifyCredentials(result.data)
-    if (!user) {
+  async login (user: UserCredentials): Promise<{ token: string }> {
+    const verifiedUser = await UserRepository.verifyCredentials(user)
+    if (!verifiedUser) {
       throw new Error('Invalid credentials')
     }
 
-    const token = signToken(user)
+    const token = signToken(verifiedUser)
 
     return { token }
   }
