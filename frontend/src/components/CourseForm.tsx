2cff31138276c31b9143066e0eb70d52af94df55
JGeanca <geancarlorivera831@gmail.com>
2025-03-08 19:57:10 -0600

    fix: change last element in list of groups on form to have rounded corners

--- frontend/src/components/CourseForm.tsx
+++ frontend/src/components/CourseForm.tsx
@@ -368,60 +368,66 @@ export default function CourseFormDialog({
                       collapsible
                       className='border rounded-md'
                     >
-                      {currentGroups.map((group, index) => (
-                        <AccordionItem
-                          key={index}
-                          value={`group-${index}`}
-                          className='border-b last:border-b-0'
-                        >
-                          <AccordionTrigger className='px-3 py-2 transition-colors group cursor-pointer'>
-                            <div className='flex items-center justify-between w-full'>
-                              <span className='text-sm font-medium text-sky-600'>
-                                {group.name}
-                              </span>
-                              <div className='flex items-center space-x-2'>
-                                <span
-                                  className='inline-flex items-center justify-center h-7 w-7 rounded-md dark:hover:bg-neutral-800'
-                                  onClick={(e) => {
-                                    e.stopPropagation()
-                                    handleEditGroup(index)
-                                  }}
-                                >
-                                  <Edit2 className='h-4 w-4 text-neutral-600' />
+                      {currentGroups.map((group, index) => {
+                        const isLast = index === currentGroups.length - 1
+                        return (
+                          <AccordionItem
+                            key={index}
+                            value={`group-${index}`}
+                            className='border-b last:border-b-0'
+                          >
+                            <AccordionTrigger className='px-3 py-2 transition-colors group cursor-pointer'>
+                              <div className='flex items-center justify-between w-full'>
+                                <span className='text-sm font-medium text-sky-600'>
+                                  {group.name}
                                 </span>
-                                <span
-                                  className='inline-flex items-center justify-center h-7 w-7 rounded-md dark:hover:bg-neutral-800'
-                                  onClick={(e) => {
-                                    e.stopPropagation()
-                                    handleDeleteGroup(index)
-                                  }}
-                                >
-                                  <Trash2 className='h-4 w-4 text-neutral-600' />
-                                </span>
-                              </div>
-                            </div>
-                          </AccordionTrigger>
-                          <AccordionContent className='px-4 py-2 bg-neutral-50 dark:bg-neutral-900 text-sm'>
-                            <div className='space-y-1'>
-                              {group.schedule
-                                .filter((s) => s.active)
-                                .map((schedule) => (
-                                  <div
-                                    key={schedule.day}
-                                    className='flex justify-between text-neutral-500 py-1 border-b last:border-b-0 '
+                                <div className='flex items-center space-x-2'>
+                                  <span
+                                    className='inline-flex items-center justify-center h-7 w-7 rounded-md dark:hover:bg-neutral-800'
+                                    onClick={(e) => {
+                                      e.stopPropagation()
+                                      handleEditGroup(index)
+                                    }}
                                   >
-                                    <span className='font-medium'>
-                                      {t(`days.${schedule.day}.short`)}
-                                    </span>
-                                    <span>
-                                      {schedule.startTime} - {schedule.endTime}
-                                    </span>
-                                  </div>
-                                ))}
-                            </div>
-                          </AccordionContent>
-                        </AccordionItem>
-                      ))}
+                                    <Edit2 className='h-4 w-4 text-neutral-600' />
+                                  </span>
+                                  <span
+                                    className='inline-flex items-center justify-center h-7 w-7 rounded-md dark:hover:bg-neutral-800'
+                                    onClick={(e) => {
+                                      e.stopPropagation()
+                                      handleDeleteGroup(index)
+                                    }}
+                                  >
+                                    <Trash2 className='h-4 w-4 text-neutral-600' />
+                                  </span>
+                                </div>
+                              </div>
+                            </AccordionTrigger>
+                            <AccordionContent
+                              className={`px-4 py-2 bg-neutral-50 dark:bg-neutral-900 text-sm ${isLast ? 'rounded-b-sm' : ''}`}
+                            >
+                              <div className='space-y-1'>
+                                {group.schedule
+                                  .filter((s) => s.active)
+                                  .map((schedule) => (
+                                    <div
+                                      key={schedule.day}
+                                      className='flex justify-between text-neutral-500 py-1 border-b last:border-b-0 '
+                                    >
+                                      <span className='font-medium'>
+                                        {t(`days.${schedule.day}.short`)}
+                                      </span>
+                                      <span>
+                                        {schedule.startTime} -{' '}
+                                        {schedule.endTime}
+                                      </span>
+                                    </div>
+                                  ))}
+                              </div>
+                            </AccordionContent>
+                          </AccordionItem>
+                        )
+                      })}
                     </Accordion>
                   ) : (
                     <div className='border rounded-md p-3 text-center text-xs text-neutral-500 italic'>
