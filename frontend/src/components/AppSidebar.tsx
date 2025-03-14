65980f85b0e1766a0516605e2c30147c7d733b1e
JGeanca <geancarlorivera831@gmail.com>
2025-03-14 14:55:08 -0600

    feat: enhance AppSidebar for mobile responsiveness and adjust layout in MainLayout

--- frontend/src/components/AppSidebar.tsx
+++ frontend/src/components/AppSidebar.tsx
@@ -11,33 +11,45 @@ import {
 import { useTranslation } from 'react-i18next'
 import CourseList from '@/components/CourseList'
 import CourseForm from './courseForm/CourseForm'
+import { useIsMobile } from '@/hooks/use-mobile'
 
 export function AppSidebar() {
   const { t } = useTranslation()
+  const isMobile = useIsMobile()
 
   return (
-    <div className='relative'>
-      <Sidebar collapsible='icon'>
-        <div className='flex items-center justify-end p-2 align-middle'>
+    <>
+      {isMobile && (
+        <div className='absolute top-2 left-2 z-10'>
           <SidebarTrigger className='cursor-pointer' />
         </div>
-        <SidebarHeader className='text-2xl font-bold px-4 py-2 group-data-[collapsible=icon]:hidden'>
-          {t('courses')}
-        </SidebarHeader>
-        <SidebarContent>
-          <div className='p-4 group-data-[collapsible=icon]:hidden'>
-            <CourseForm />
-          </div>
-          <SidebarGroup className=' pt-10 overflow-y-auto'>
-            <SidebarGroupLabel className='px-4 group-data-[collapsible=icon]:hidden'>
-              {t('Lista de cursos')}
-            </SidebarGroupLabel>
-            <SidebarGroupContent>
-              <CourseList />
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
-    </div>
+      )}
+
+      <div className={isMobile ? 'invisible sm:visible' : ''}>
+        <Sidebar collapsible='icon'>
+          {!isMobile && (
+            <div className='flex items-center justify-end p-2 align-middle'>
+              <SidebarTrigger className='cursor-pointer' />
+            </div>
+          )}
+          <SidebarHeader className='text-2xl font-bold px-4 py-2 group-data-[collapsible=icon]:hidden'>
+            {t('courses')}
+          </SidebarHeader>
+          <SidebarContent>
+            <div className='p-4 group-data-[collapsible=icon]:hidden'>
+              <CourseForm />
+            </div>
+            <SidebarGroup className='pt-10 overflow-y-auto'>
+              <SidebarGroupLabel className='px-4 group-data-[collapsible=icon]:hidden'>
+                {t('Lista de cursos')}
+              </SidebarGroupLabel>
+              <SidebarGroupContent>
+                <CourseList />
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
+      </div>
+    </>
   )
 }
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -7,7 +7,9 @@ const MainLayout = () => {
   return (
     <div className='flex h-screen dark:bg-neutral-800 bg-neutral-100'>
       <SidebarProvider>
-        <AppSidebar />
+        <div className='relative'>
+          <AppSidebar />
+        </div>
         <div className='flex-1 flex flex-col overflow-hidden'>
           <header className='p-4 border-b dark:border-neutral-700'>
             <div className='flex items-center justify-between'>
