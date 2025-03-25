601772f314494d105b73e02dd55a05d1f3dc4654
JGeanca <geancarlorivera831@gmail.com>
2025-03-24 18:01:54 -0600

    feat: add skeleton loader to EmptyScheduleTable for loading state

--- frontend/src/components/EmptyScheduleTable.tsx
+++ frontend/src/components/EmptyScheduleTable.tsx
@@ -1,10 +1,63 @@
 import { DAYS, TIME_RANGES } from '@/utils/constants'
 import { useTranslation } from 'react-i18next'
+import useScheduleStore from '@/stores/useScheduleStore'
 
 const EmptyScheduleTable = () => {
   const { t } = useTranslation()
+  const { isLoading } = useScheduleStore()
 
-  return (
+  const SkeletonLoader = () => (
+    <div className='mb-12'>
+      <div className='overflow-x-auto md:overflow-visible'>
+        <div className='min-w-[600px] md:min-w-0'>
+          <table className='w-full border-collapse'>
+            <thead>
+              <tr>
+                <th className='border border-neutral-900 dark:border-neutral-300 w-16 md:w-24 h-9 rounded-sm'>
+                  {t('hours')}
+                </th>
+                {DAYS.map((day) => (
+                  <th
+                    key={day}
+                    className='p-1 border border-neutral-900 dark:border-neutral-300 w-20 md:w-32 rounded-sm'
+                  >
+                    ({t(`days.${day}.short`)}) {t(`days.${day}.name`)}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {TIME_RANGES.map((range) => (
+                <tr key={range}>
+                  <td className='p-1 border border-neutral-900 dark:border-neutral-300 text-center w-16 md:w-24 rounded-sm'>
+                    {range}
+                  </td>
+                  {DAYS.map((day) => (
+                    <td
+                      key={`${day}-${range}`}
+                      className='border border-neutral-900 dark:border-neutral-300 w-20 md:w-24 h-9 p-1 rounded-sm'
+                    >
+                      <div className='h-full w-full relative overflow-hidden rounded-sm'>
+                        <div
+                          className='absolute inset-0 
+                          bg-neutral-200 dark:bg-neutral-700/80 
+                          animate-pulse rounded-b-xs'
+                        ></div>
+                      </div>
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+
+  return isLoading ? (
+    <SkeletonLoader />
+  ) : (
     <div className='mb-12'>
       <div className='overflow-x-auto md:overflow-visible'>
         <div className='min-w-[600px] md:min-w-0'>
