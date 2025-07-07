22c026b71140e7d40726b8825d6e0204ef2baacb
gqbo <gabriel.gonzaf@gmail.com>
2025-07-07 02:47:18 -0600

    fix: update allowed origins to include www.horariu.com

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,11 @@ import { UserController } from './controllers/userController'
 import { CourseController } from './controllers/courseController'
 import { UnauthorizedError } from './utils/customsErrors'
 
-const allowedOrigins = ['http://localhost:5173', 'https://horariu.com']
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://horariu.com',
+  'https://www.horariu.com'
+]
 
 const getCorsHeaders = (origin: string | undefined, methods: string) => {
   const allowedOrigin = origin && allowedOrigins.includes(origin) ? origin : ''
